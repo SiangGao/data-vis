1ea26ae24cc53cf3a21e570143c5211984bd1027
gaosa <isgaosa@gmail.com>
2019-11-27 16:14:28 -0500

    Remove useless stuff

--- js/index.js
+++
@@ -1,5 +0,0 @@
-const eles = document.getElementsByClassName('full-screen');
-for (let i = 0; i < eles.length; ++i) {
-    eles[i].setAttribute("style","height:" + window.innerHeight + "px");
-    eles[i].style.height = window.innerHeight + "px";
-}
