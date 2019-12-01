6afbc5adb8fbb8c4c9c3a74948a6f11a960a3663
SiangGao <gsa007@126.com>
2019-11-30 20:39:56 -0500

    Update casualty-count.js

--- js/casualty-count.js
+++ js/casualty-count.js
@@ -15,7 +15,7 @@ let individual_incident_p5 = new p5(function(p) {
     const boundaryColor = "rgb(172, 18, 20)";
     const fillColor = "rgba(172, 18, 20, 0.6)";
     const parentDiv = document.getElementById("casualty-count-div");
-    const dotWidth = 20;
+    const dotWidth = 15;
 
     p.setup = function() {
         const canvas = p.createCanvas(parentDiv.clientWidth, parentDiv.clientHeight);
@@ -146,10 +146,10 @@ let individual_incident_p5 = new p5(function(p) {
     }
 
     p.drawYears = function(xlo, xhi, y) {
-        p.textAlign(p.CENTER, p.CENTER);
+        p.textAlign(p.CENTER, p.TOP);
         p.stroke("black");
         p.fill(boundaryColor);
-        p.strokeWeight(1);
+        p.strokeWeight(2);
         p.textSize(20);
         for (let i = 0; i <= 2017 - 1970; i += 5) {
             p.text(1970 + i, xlo + (xhi - xlo) / (2017 - 1970) * i, y);
@@ -160,13 +160,13 @@ let individual_incident_p5 = new p5(function(p) {
         p.clear();
         //p.background('black');
         const marginh = 40, marginv = 70;
-                
+        
         const d = p.mouseX - marginh + dotWidth/2;
         const w = (p.width - 2 * marginh) / (2017 - 1970);
         const idx = Math.floor(d / w);
 
         if (!(idx >= 0 && idx <= 47)) {
-            p.drawYears(marginh, p.width - marginh, p.height / 2 + 10);
+            p.drawYears(marginh, p.width - marginh, p.height - 40);
         }
         p.drawData(marginh, p.width - marginh, marginv, p.height / 2 - 10, true, [ 'line', 'dots']);
         p.drawData(marginh, p.width - marginh, p.height / 2 + 10, p.height - marginv, false, ['line', 'dots']);
